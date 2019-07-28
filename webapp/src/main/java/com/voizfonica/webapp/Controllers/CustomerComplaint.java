278d628e48304b86f00b30660a04bcde6e4b3b68
Nandhini Vishwanathan <nandhinivishwanathanbe@gmail.com>
2019-07-28 21:51:48 +0530

    Done by Nandhini

--- webapp/src/main/java/com/voizfonica/webapp/Controllers/CustomerComplaint.java
+++ webapp/src/main/java/com/voizfonica/webapp/Controllers/CustomerComplaint.java
@@ -1,13 +1,23 @@
 package com.voizfonica.webapp.Controllers;
 
+import org.springframework.context.annotation.Configuration;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Controller
-public class CustomerComplaint {
+@Configuration
+@EnableWebMvc
+public class CustomerComplaint implements WebMvcConfigurer {
+    public void addResourceHandlers(final ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/resources/**").addResourceLocations("/resources/");
+    }
     @GetMapping("/")
     public String query()
     {
         return "customerComplaint";
     }
+
 }
